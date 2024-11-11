1f61410a4680a3a6f7d45a4e09497fa4b52704e2
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2024-11-11 17:27:00 +0530

    update

--- portfolio/src/component/Project.jsx
+++ portfolio/src/component/Project.jsx
@@ -9,24 +9,24 @@ const Project = () => {
 
         <div className="space-y-6">
           <div>
-            <h3 className="text-yellow-600 font-bold text-xl">1. YouTube</h3>
-            <p className="text-lg font-bold text-yellow-900">It is a clone of YouTube with all the functionality and based on the latest technology.</p>
+            <h3 className="text-yellow-600 font-bold text-xl">1. Chat application</h3>
+            <p className="text-lg font-bold text-yellow-900">it is Full Stack Project in which you can send and read messages. it is fully authenticated and your personal data is saved and every user has it own data which is secured from other only you can acces it. you can message them separately.</p>
           </div>
           
-          <div>
+          {/* <div>
             <h3 className="text-yellow-600 font-bold text-xl">2. Netflix</h3>
             <p className="text-lg font-bold text-yellow-900">
               It is a clone of Netflix, secured with Firebase, and has a very attractive user interface. It covers all the features and creates a list of the latest movies. It is different from Netflix in terms of GPT search and AI-based suggestions according to user input.
             </p>
-          </div>
+          </div> */}
           
           <div>
             <h3 className="text-yellow-600 font-bold text-xl">3. Storing Your Contact Info</h3>
-            <p className="text-lg font-bold text-yellow-900">A backend project based on MongoDB, implementing all CRUD operations.</p>
+            <p className="text-lg font-bold text-yellow-900">A backend project based on MongoDB,Python,Docker,and Flask where we are implementing all CRUD operations.</p>
           </div>
           
           <div>
-            <h3 className="text-yellow-600 font-bold text-xl">4. Backend Project</h3>
+            <h3 className="text-yellow-600 font-bold text-xl">4. Registeration Page</h3>
             <p className="text-lg font-bold text-yellow-900">
               This project fully authorizes your website using cookies and JWT tokens. Pages cannot be accessed without user login.
             </p>
@@ -34,7 +34,7 @@ const Project = () => {
           <div>
             <h3 className="text-yellow-600 font-extrabold text-xl ">5.Ecomerence website</h3>
             <p className="text-lg font-bold text-yellow-900">
-              This project fully authorizes your website using cookies and JWT tokens. Pages cannot be accessed without user login.
+              it is basically a frontend project where we make all the functionality of Ecommerence website just like adding items to Cart etc.
             </p>
           </div>
         </div>
